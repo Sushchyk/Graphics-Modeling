d01793ff639f9b582a93cefc702586dc05fb0892
Andriy Sushchyk <sushchyk12@gmail.com>
2017-02-19 23:52:38 +0200

    Change colors

--- Lab1/ColorsProvider.java
+++ Lab1/ColorsProvider.java
@@ -1,8 +1,8 @@
 class ColorsProvider {
 
-    final static String backgroundColor = "#000000";
-    final static String boomColor = "#FF0000";
-    final static String boatColor = "#444444";
-    final static String sailColor = "#555555";
+    final static String backgroundColor = "#FFFF80";
+    final static String boomColor = "#000000";
+    final static String boatColor = "#804000";
+    final static String sailColor = "#C0C0C0";
 
 }
