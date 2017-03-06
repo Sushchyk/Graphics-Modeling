a53028297152303493d06bcc23d1cbd1d5655d50
Andriy Sushchyk <sushchyk12@gmail.com>
2017-03-06 19:46:13 +0200

    Edit ShipSceneBorder class

--- Lab2/src/ShipSceneBorder.java
+++ Lab2/src/ShipSceneBorder.java
@@ -3,23 +3,31 @@ import java.awt.*;
 public class ShipSceneBorder implements SceneBorder {
 
     private Color borderColor;
-
+    private int borderThickness = 0;
+    private int borderMargin = 0;
     private final static int DEFAULT_BORDER_MARGIN = 10;
     private final static int DEFAULT_BORDER_SIZE = 4;
 
     private Dimension frameSize;
+
+    public void setBorderSize(int thickness, int margin) {
+        this.borderThickness = thickness;
+        this.borderMargin = margin;
+    }
 
     public ShipSceneBorder(Dimension frameSize, Color color) {
         this.borderColor = color;
         this.frameSize = frameSize;
     }
 
+    private BasicStroke getBorderStroke() {
+        return new BasicStroke(DEFAULT_BORDER_SIZE,
+                BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL);
+    }
+
     @Override
     public void drawBorder(Graphics2D g2d) {
-        BasicStroke miterStroke = new BasicStroke(DEFAULT_BORDER_SIZE,
-                BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL);
-        g2d.setStroke(miterStroke);
-
+        g2d.setStroke(getBorderStroke());
         g2d.setColor(this.borderColor);
 
         g2d.drawRect(DEFAULT_BORDER_MARGIN, DEFAULT_BORDER_MARGIN,
