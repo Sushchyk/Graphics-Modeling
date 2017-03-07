06191b1d27be35641fb4190738ea0d1e71475d30
Andriy Sushchyk <sushchyk12@gmail.com>
2017-03-07 22:28:50 +0200

    Upload Lab3

---
+++ Lab3/src/CircleCreator.java
@@ -0,0 +1,10 @@
+import javafx.scene.shape.Circle;
+
+public class CircleCreator extends ShapeCreator {
+
+    @Override
+    protected Circle fillShapeByPoints(Double[] points) {
+        Circle circle = new Circle(points[0], points[1], points[2]);
+        return circle;
+    }
+}
---
+++ Lab3/src/PolygonCreator.java
@@ -0,0 +1,13 @@
+import javafx.scene.shape.Polygon;
+
+public class PolygonCreator extends ShapeCreator {
+
+    @Override
+    protected Polygon fillShapeByPoints(Double[] points) {
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(points);
+        return polygon;
+    }
+
+
+}
